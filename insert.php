b048dd009454d8bd91e1e0fb34c1072c0800783b
Jitendra Mahapatra <jmahapatra@live.com>
2017-07-24 23:15:18 +0530

    testing img upload

--- insert.php
+++ insert.php
@@ -1,9 +1,9 @@
 <?php
-require_once('config/config.php');
-
+//require_once('config/config.php');
+print_r($_FILES);
 
    //select collection
-   $collection = $db->items;
+   //$collection = $db->items;
 
    //assigning variables
    $itemname = $_POST['itemname'];
@@ -12,7 +12,10 @@ require_once('config/config.php');
    foreach($_POST['buffet'] as $key=>$value){
      $buffet[] = array("type" => $value, "timing" => $_POST['timing'][$key]);
    }
+   $file = $_FILES['tmp_name'];
    $cusins = $_POST['cuisin'];
+   
+
 
    //insert data
    $doc = array(
@@ -20,9 +23,10 @@ require_once('config/config.php');
       "type" => $type,
       "ingredients" => $ingredients,
       "buffet" => $buffet,
-      "cuisins" => $cusins
+      "cuisins" => $cusins,
+      "photo" => new MongoBinData(file_get_contents($file))
   );
 
-   $collection->insert($doc);
-   header("addproduct.html");
+   //$collection->insert($doc);
+   //header("addproduct.html");
 ?>
